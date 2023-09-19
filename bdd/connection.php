9e85ce3fe15329410fabc50abce18f5b23423c24
Kevin <116793967+KevinML48@users.noreply.github.com>
2023-09-19 04:04:28 +0200

    remise a niveau

--- bdd/connection.php
+++ bdd/connection.php
@@ -1,6 +1,6 @@
 <?php
 // Informations de connexion à la base de données
-$serveur = "erah.great-site.net";  // Adresse du serveur MySQL
+$serveur = "localhost";  // Adresse du serveur MySQL
 $utilisateur = "root"; // Nom d'utilisateur MySQL
 $motDePasse = ""; // Mot de passe MySQL
 $baseDeDonnees = "erah"; // Nom de la base de données
